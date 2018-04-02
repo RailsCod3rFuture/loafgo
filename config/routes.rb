47101bed314d3c0caa3cdf82032b858214cd120f
admin <admin>
2018-04-02 16:12:27 -0400

    Removed unnecessary 'manager_order_tracker_controller' get statements from routes. Replaced them with controller: module Removed app_documentation controller module. Because app documentation will be placed in the markdown view within the home_controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,12 @@
 Rails.application.routes.draw do
-  get 'manager_order_tracker/index'
 
-  get 'manager_order_tracker/edit'
-
-  get 'manager_order_tracker/show'
-
-  controller :app_documentation do
-    get '/software_documentation', to: 'app_documentation#index', as: :software_documentation
-  end
-
-  devise_for :managers, :controllers => {
-      :registrations => "managers/registrations",
-      :passwords => "managers/passwords"
+  devise_for :managers, controllers: {
+      registrations: 'managers/registrations',
+      passwords: 'managers/passwords'
   }
-  devise_for :clients, :controllers => {
-      :registrations => "clients/registrations",
-      :passwords => "clients/passwords"
+  devise_for :clients, controllers: {
+      registrations: 'clients/registrations',
+      passwords: 'clients/passwords'
   }
   controller :home do
     get 'home/index', to: 'home#index', as: 'home'
@@ -33,25 +24,26 @@ Rails.application.routes.draw do
   end
 
   controller :manager_dashboard do
-    get 'm_dashboard', to: 'manager_dashboard#index', :as => :manager_dashboard
-  end
-
-  controller :client_dashboard do
-    get 'c_dashboard', to: 'client_dashboard#index', :as => :client_dashboard
-  end
-  controller :order_finder do
-    get 'order_finder', to: 'order_finder#index', :as => :order_finder
+    get 'm_dashboard', to: 'manager_dashboard#index', as: :manager_dashboard
   end
 
   controller :manager_order_tracker do
-    get 'manager_order_trackers', to: 'manager_order_tracker#index', :as => :manager_order_trackers
+    get 'manager_order_tracker', to: 'manager_order_tracker#index', as: :manager_order_trackers_path
   end
+
+  controller :client_dashboard do
+    get 'c_dashboard', to: 'client_dashboard#index', as: :client_dashboard
+  end
+  controller :order_finder do
+    get 'order_finder', to: 'order_finder#index', as: :order_finder
+  end
+
   resources :deliveries
   resources :orders
   resources :order_feedbacks
   resources :order_trackers
   resources :trucks
-  resource :warehouse, only: [:edit, :show, :update]
+  resource :warehouse, only: %i[edit show update]
 
   root to: 'home#index'
 end
