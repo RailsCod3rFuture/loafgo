183882130f5b49375f9d9a4ff53c3f8c99780318
admin <admin>
2018-04-04 13:26:36 -0400

    Fixed search form for order_finder controller Removed bread_type from order Removed edit view from manager_order_tracker / reverts to normal order tracker view/controller upon updating/inserting new record Added no results partial for order_finder search engine Updated git ignore to stop loafgo.iml configuration file from being included in commits. (will force you to re-initialize the ruby gemset for the project!

--- app/controllers/order_finder_controller.rb
+++ app/controllers/order_finder_controller.rb
@@ -2,7 +2,12 @@ class OrderFinderController < ApplicationController
   before_action :authenticate_manager!
 
   def index
+    if params[:q] && params[:q].reject {|k, v| v.blank?}.present?
     @q = Order.ransack(params[:q])
-    @orders = @q.result.includes(:clients).page(params[:page])
+    @orders = @q.result.page(params[:page])
+    else
+      @q = Order.ransack
+      @orders = []
+    end
   end
 end
